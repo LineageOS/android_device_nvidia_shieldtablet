3a8e084cb5668a58b589023b5e4c59a310e61a27
C457 <android.c357@gmail.com>
2014-08-26 18:34:09 -0500

    shieldtablet: Bluedroid vendor config
    The nvidia shield tablet requires a special configuration for
    enabling bluetooth.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -60,6 +60,7 @@ USE_OPENGL_RENDERER := true
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/nvidia/shieldtablet/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/nvidia/shieldtablet/bluetooth/vnd_shieldtablet.txt
 
 # Wifi related defines
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
