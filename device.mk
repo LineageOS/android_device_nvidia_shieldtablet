04103726aa560a6fddcde98c6fd263ce564a8265
Aaron Kling <webgeek1234@gmail.com>
2015-07-24 12:03:49 -0500

    New way of including blake support

--- device.mk
+++ device.mk
@@ -163,5 +163,5 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
 # Wireless Controller
-PRODUCT_PACKAGES += BlakePairing
+$(call inherit-product-if-exists, vendor/nvidia/shield_common/blake-blobs.mk)
 
