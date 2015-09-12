05442b978d6ffd41c2ca196849e09d6185a4c2d0
Aaron Kling <webgeek1234@gmail.com>
2015-09-11 21:33:38 -0500

    Enable selinux enforcing mode

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,6 @@ TARGET_NO_RADIOIMAGE := true
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/nvidia/shieldtablet
 TARGET_KERNEL_CONFIG := cyanogenmod_shieldtablet_defconfig
-BOARD_KERNEL_CMDLINE := "androidboot.selinux=permissive"
 
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_USERDATAIMAGE_PARTITION_SIZE  := 12799754240
