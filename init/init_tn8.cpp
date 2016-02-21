d4db8e963549f310e4d48ff9cdc1da628f96bb5b
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-21 02:57:44 +0200

    shieldtablet: Allow vendor init to override props
    Change-Id: Ic7c4d86616bfe33b9668b5b54d119d0380f4660c

--- init/init_tn8.cpp
+++ init/init_tn8.cpp
@@ -27,6 +27,8 @@
 
 #include <stdlib.h>
 #include <errno.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
 #include "init.h"
 #include "vendor_init.h"
@@ -36,6 +38,17 @@
 #include "service.h"
 #include <stdio.h>
 #include <string.h>
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
 void gsm_properties()
 {
