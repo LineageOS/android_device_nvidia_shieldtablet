b0328c9f121146740adba1857f483ecaf8d7eae7
Aaron Kling <webgeek1234@gmail.com>
2018-06-30 11:57:53 -0500

    Fix alternate build
    Change-Id: I34fd49f392abea9ccebdc2f03425c6e84c8f3432

--- device_atv.mk
+++ device_atv.mk
@@ -1,5 +1,1 @@
 PRODUCT_CHARACTERISTICS := tv
-
-PRODUCT_PROPERTY_OVERRIDES += persist.tegra.hdmi.primary=1 \
-                              persist.tegra.stb.mode=1 \
-                              persist.tegra.hdmi.resolution=Max
