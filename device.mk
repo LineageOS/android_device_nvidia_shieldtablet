2bbbc50e9e990d1b4e645f8e6b7815ab96c05547
Bruno Martins <bgcngm@gmail.com>
2018-08-10 23:14:57 +0100

    Import stock Dalvik heap overrides
    Change-Id: Ifac6650a874cf982a6e767da92599d4b6230a637

--- device.mk
+++ device.mk
@@ -47,8 +47,6 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT     := 1920
 TARGET_SCREEN_WIDTH      := 1200
 
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-
 $(call inherit-product, vendor/nvidia/shieldtablet/shieldtablet-vendor.mk)
 
 # Overlays
