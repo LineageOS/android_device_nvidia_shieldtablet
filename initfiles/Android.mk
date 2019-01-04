0e8d0575c5b3870696cd27cd5c138762d2ac3c6e
Aaron Kling <webgeek1234@gmail.com>
2019-01-03 22:18:23 -0600

    Add loki support
    Change-Id: I62cfd36a68e2897caaf96167e0aee12fa0f3ec66

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -1,6 +1,13 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE        := fstab.loki
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := fstab.tn8
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE        := fstab.tn8
 LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := fstab.tn8
@@ -8,10 +15,25 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.loki.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.recovery.loki.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.tn8.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.recovery.tn8.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := init.loki.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := init.loki.rc
+LOCAL_VENDOR_MODULE        := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -28,6 +50,13 @@ LOCAL_MODULE_CLASS         := ETC
 LOCAL_SRC_FILES            := init.tn8_common.rc
 LOCAL_VENDOR_MODULE        := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := power.loki.rc
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := power.tn8.rc
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
