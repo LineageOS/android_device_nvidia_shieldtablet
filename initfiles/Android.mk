6b5be8295711a1bedcd114067241df50a3c2aa42
Aaron Kling <webgeek1234@gmail.com>
2019-01-03 22:27:35 -0600

    Add ardbeg support
    Change-Id: I620e52745735f3b1b45a0c71fc648b38b37a5ef9

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -1,6 +1,13 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE        := fstab.ardbeg
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := fstab.tn8
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE        := fstab.loki
 LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := fstab.tn8
@@ -15,6 +22,13 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.ardbeg.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.recovery.ardbeg.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.loki.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.recovery.loki.rc
@@ -26,6 +40,14 @@ LOCAL_MODULE       := init.recovery.tn8.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.recovery.tn8.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := init.ardbeg.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := init.ardbeg.rc
+LOCAL_VENDOR_MODULE        := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -50,6 +72,13 @@ LOCAL_MODULE_CLASS         := ETC
 LOCAL_SRC_FILES            := init.tn8_common.rc
 LOCAL_VENDOR_MODULE        := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := power.ardbeg.rc
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := power.ardbeg.rc
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
