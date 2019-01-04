be99a378e53fd60252caf8eb4635dac6ad533f61
Aaron Kling <webgeek1234@gmail.com>
2019-01-04 14:16:07 -0600

    Inherit raydium
    Most variants have a rm31080 touchscreen
    
    Change-Id: I56ce2bc47bff2ff650bb3b9e4273766b4ad8158e

--- device.mk
+++ device.mk
@@ -16,6 +16,7 @@
 
 $(call inherit-product, device/nvidia/t124-common/t124.mk)
 $(call inherit-product, device/nvidia/icera/icera.mk)
+$(call inherit-product, device/nvidia/touch/raydium.mk)
 
 PRODUCT_CHARACTERISTICS  := tablet
 PRODUCT_AAPT_CONFIG      := xlarge large
