54c76dbb105ba4fc1b980516aaec93d1e8e36c12
Aaron Kling <webgeek1234@gmail.com>
2019-01-04 16:15:36 -0600

    Add wifi hal and configs
    Most variants use a bcm43241
    
    WIP: TODO: Handle yellowstones 43341
    Change-Id: I9bed2c6625b94560c6fba708fcca9e48fb676d71

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,9 @@
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.bt.bdaddr_path=/mnt/vendor/factory/bluetooth/bt_mac.txt
+
+# WiFi
+PRODUCT_PROPERTY_OVERRIDES += \
+   ap.interface=wlan0 \
+   wifi.direct.interface=p2p-dev-wlan0 \
+   wifi.interface=wlan0
