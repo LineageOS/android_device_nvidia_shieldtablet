3caeb27ed0359bca5180194ca4226cb9b55b14af
Aaron Kling <webgeek1234@gmail.com>
2019-01-04 14:32:32 -0600

    Add bluetooth hal and configs
    Change-Id: I90497a93046d6dd67e6de9dad11d5750dbb5713d

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,7 @@
 # Adb compat for older kernels
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.usb.ffs.aio_compat=1
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bt.bdaddr_path=/mnt/factory/bluetooth/bt_mac.txt
