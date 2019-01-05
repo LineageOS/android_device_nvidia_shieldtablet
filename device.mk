7e89f67c76f1e22a063c60ce561b687580acb817
Aaron Kling <webgeek1234@gmail.com>
2019-01-05 10:30:33 -0600

    Add shield-common references
    This is an Nvidia Shield device
    
    Change-Id: I1431154b074ae75c064c36d591192286c760c8d8

--- device.mk
+++ device.mk
@@ -17,6 +17,7 @@
 $(call inherit-product, device/nvidia/t124-common/t124.mk)
 $(call inherit-product, device/nvidia/icera/icera.mk)
 $(call inherit-product, device/nvidia/touch/raydium.mk)
+$(call inherit-product, device/nvidia/shield-common/shield.mk)
 
 PRODUCT_CHARACTERISTICS  := tablet
 PRODUCT_AAPT_CONFIG      := xlarge large
