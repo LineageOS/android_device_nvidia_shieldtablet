51329c3c4db60c10bbf725dd7f3ee353cf37645e
Aaron Kling <webgeek1234@gmail.com>
2019-01-05 16:29:00 -0600

    Add audio configs
    WIP: TODO: Double check these. effects may be unmodified from aosp
    Change-Id: I61a0514a303c9d275278c1dfc3edd2fe346061bc

--- device.mk
+++ device.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+TARGET_TEGRA_AUDIO ?= nvaudio
+
 $(call inherit-product, device/nvidia/t124-common/t124.mk)
 $(call inherit-product, device/nvidia/icera/icera.mk)
 $(call inherit-product, device/nvidia/touch/raydium.mk)
@@ -69,6 +71,16 @@ ifneq ($(PRODUCT_IS_ATV),true)
 	    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
 	    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
 	    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+endif
+
+# Audio
+ifeq ($(TARGET_TARGET_TEGRA_AUDIO),nvaudio)
+PRODUCT_PACKAGES += \
+    audioConfig_qvoice_icera_pc400.xml \
+    audio_effects.conf \
+    audio_policy.conf \
+    nvaudio_conf.xml \
+    nvaudio_fx.xml
 endif
 
 # Camera
