84fe9676028b828fc592d5a0c9a1bbd59c9f03d9
Aaron Kling <webgeek1234@gmail.com>
2019-01-05 15:58:54 -0600

    Add a few tablet specific permission files
    Platform tablet and handheld, plus one for touchscreen
    
    Change-Id: I9197aca095a7c74e1ade9e813a7452ad0213a68d

--- device.mk
+++ device.mk
@@ -67,6 +67,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# Only for tablet builds
+ifneq ($(PRODUCT_IS_ATV),true)
+	PRODUCT_COPY_FILES += \
+	    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+endif
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
