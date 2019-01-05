1b3fee63f32681e4061f62f54dae8928ff4e3f34
Aaron Kling <webgeek1234@gmail.com>
2019-01-05 16:49:32 -0600

    Add TWRP support
    Change-Id: Ic636a8c3e96b297ee64704fa47668975ca4b01e0

---
+++ twrp/twrp.mk
@@ -0,0 +1,6 @@
+PRODUCT_COPY_FILES += device/nvidia/shieldtablet/twrp/twrp.fstab:recovery/root/etc/twrp.fstab
+
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_BRIGHTNESS_PATH := /sys/class/backlight/pwm-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
