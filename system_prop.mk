105afe72d256c4c434446ce749b27ed5f406ca55
Aaron Kling <webgeek1234@gmail.com>
2019-01-05 19:23:15 -0600

    Add smartdimmer properties
    Change-Id: Iaadfb726dd4163918aaadea29f4030ded10e2c4c

--- system_prop.mk
+++ system_prop.mk
@@ -2,6 +2,12 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.bt.bdaddr_path=/mnt/factory/bluetooth/bt_mac.txt
 
+# Smartdimmer
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.tegra.didim.enable = 1 \
+    persist.tegra.didim.video = 5 \
+    persist.tegra.didim.normal = 3
+
 # WiFi
 PRODUCT_PROPERTY_OVERRIDES += \
    ap.interface=wlan0 \
