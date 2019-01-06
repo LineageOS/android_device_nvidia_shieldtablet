94c19e0be3ae582dcd940914235fc06da329efb6
Aaron Kling <webgeek1234@gmail.com>
2019-01-05 19:23:15 -0600

    Add smartdimmer properties
    Change-Id: Iaadfb726dd4163918aaadea29f4030ded10e2c4c

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,9 @@
+# Smartdimmer
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.tegra.didim.enable = 1 \
+    persist.tegra.didim.video = 5 \
+    persist.tegra.didim.normal = 3
+
 # WiFi
 PRODUCT_PROPERTY_OVERRIDES += \
    ap.interface=wlan0 \
