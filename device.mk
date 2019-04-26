5faea6af024ffdecc6a21547e03f59c7a160c119
Aaron Kling <webgeek1234@gmail.com>
2019-04-26 09:32:20 -0500

    Add jetson-tk1 support
    Change-Id: Ie98b16d30cf53ad680b09df14003fee702fbdd99

--- device.mk
+++ device.mk
@@ -30,16 +30,20 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 # Init related
 PRODUCT_PACKAGES += \
     fstab.ardbeg \
+    fstab.jetson-tk1 \
     fstab.loki \
     fstab.tn8 \
     init.recovery.ardbeg.rc \
+    init.recovery.jetson-tk1.rc \
     init.recovery.loki.rc \
     init.recovery.tn8.rc \
     init.ardbeg.rc \
+    init.jetson-tk1.rc \
     init.loki.rc \
     init.tn8.rc \
     init.tn8_common.rc \
     power.ardbeg.rc \
+    power.jetson-tk1.rc \
     power.loki.rc \
     power.tn8.rc \
     ueventd.tn8.rc
