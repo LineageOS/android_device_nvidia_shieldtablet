e5c5944d3d102ac804ec3f8ba2a91db4203b0a03
Nolen Johnson <johnsonnolen@gmail.com>
2020-04-14 17:40:02 -0500

    Kang LMKD configuration from bonito
    Change-Id: Ie36a85cfa287d97cc074d0d1a66ee1e70bc9ed6e

--- system_prop.mk
+++ system_prop.mk
@@ -2,6 +2,18 @@
 PRODUCT_PROPERTY_OVERRIDES += \
    ro.recents.grid=true
 
+# LMKd
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true
+
 # Smartdimmer
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.tegra.didim.enable = 1 \
