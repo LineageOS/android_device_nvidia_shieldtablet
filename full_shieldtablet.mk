c8a9940b5a391367db262d54fd7d5beb708f9245
Aaron Kling <webgeek1234@gmail.com>
2020-12-11 15:06:40 -0600

    Build telephony target
    This will include rild and other cell support packages. Needed for
    the lte variants.
    
    Change-Id: I9bc9da2f3cecb94f88f422eccd73407b1487010c

--- full_shieldtablet.mk
+++ full_shieldtablet.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/nvidia/shieldtablet/device.mk)
 
 # Set those variables here to overwrite the inherited values.
